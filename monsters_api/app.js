c8db0b8031136d52524dbdebef15b8853f6b588d
TailaneBrito <tailane.brito01@gmail.com>
2021-02-07 21:06:16 -0300

    error Handling

--- monsters_api/app.js
+++ monsters_api/app.js
@@ -4,13 +4,18 @@ const pool = require('./db');
 const app = express();
 
 //arrow function call back function
-app.get('/monsters', (request, response) => {
-    pool.query('SELECT * FROM monsters ORDER BY id ASC', (err, res) => {
-        if (err) return console.log(err);
+app.get('/monsters', (request, response, next) => {
+    pool.query('SELECT * FROM foo ORDER BY id ASC', (err, res) => {
+        if (err) return next(err);
 
         response.json(res.rows);
     });
 
+});
+
+//middlewares
+app.use((err, req, res, next) => {
+    res.json(err);
 });
 
 module.exports = app;
